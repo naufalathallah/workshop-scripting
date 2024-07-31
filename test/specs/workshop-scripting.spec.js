905a5af3cf66f9b4ab513f01e636ce4157ec72da
Naufal A <spealdm6@gmail.com>
2024-07-31 13:48:28 +0700

    fix: add hint

--- test/specs/workshop-scripting.spec.js
+++ test/specs/workshop-scripting.spec.js
@@ -1,8 +1,5 @@
 describe("Deskripsi Fitur", () => {
   it("[key: TCID] Judul Test Case", async () => {
-    await $(loginPage.jpopsRekamMedis).click();
-
-    expect($(rekamMedisPage.pageRekamMedis)).toBeDisplayed();
-    expect($(rekamMedisPage.cardRekamMedisBaby)).toBeDisplayed();
+    // Code disini ya...
   });
 });
