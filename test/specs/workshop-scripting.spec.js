870b32334db39d456470ade3ab6f223e75b88b95
Naufal A <spealdm6@gmail.com>
2024-07-31 14:06:47 +0700

    fix: add pause and log

--- test/specs/workshop-scripting.spec.js
+++ test/specs/workshop-scripting.spec.js
@@ -1,5 +1,7 @@
 describe("Deskripsi Fitur", () => {
   it("[key: TCID] Judul Test Case", async () => {
     // Code disini ya...
+    console.log("===============\nDone Setup Running");
+    await driver.pause(5000);
   });
 });
